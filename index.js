de04340a7c3b31d87a5e0b5b48d234a4b59db980
jstriebel <jstriebel@users.noreply.github.com>
2015-04-15 09:22:00 +0800

    setting panorama always to full height, refactored

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
 /*jslint browser:true */
 
+function setToFullHeight(el) {
+    var windowHeight = $(window).height();
+    el.css('height', windowHeight + 'px');
+}
+
 $(document).ready(function ($) {
 
     var url = window.location.hash.substring(1);
 
     if (url) {
-        $('.panoView').removeClass('vanished');
-        var tmpImg = new Image();
-        tmpImg.src = url;
+        var pano = $('#pano');
 
-        $(tmpImg).one('load', function () {
-
-            function setImageHeight() {
-                var windowHeight = $(window).height();
-                var height = tmpImg.height;
-                if (height > windowHeight) {
-                    height = windowHeight;
-                }
-                $('#pano').css('height', height + 'px');
-                $('#pano').css('background-size', 'auto ' + height + 'px');
-            }
-
-            setImageHeight();
-            $(window).resize(setImageHeight);
-
-            $('#pano').css('background-image', 'url(' + url + ')');
-            $('#pano').css('cursor', 'move');
-            $('#pano').cyclotron();
+        setToFullHeight(pano);
+        pano.css('background-size', 'cover');
+        $(window).resize(function () {
+            setToFullHeight(pano);
         });
+        $('.panoView').removeClass('vanished');
 
+        pano.css('background-image', 'url("' + url + '")');
+        pano.css('cursor', 'move');
+        pano.cyclotron();
     } else {
         $('.mainView').removeClass('vanished');
     }
